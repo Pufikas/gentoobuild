4ffa6dbc08b7f9767212ee370834bcc0123a7cb5
Pufikas <dominykas.guzys@gmail.com>
2022-12-03 22:17:41 +0200

    Update manual.sh

--- manual.sh
+++ manual.sh
@@ -146,3 +146,38 @@ emerge --ask net-misc/dhcpcd
 # enable it
 rc-update add dhcpcd default
 rc-service dhcpcd start
+# emerge netifirc
+emerge --ask --noreplace net-misc/netifrc
+# start at boot
+cd /etc/init.d
+ln -s net.lo net.enp0s3
+rc-update add net.enp0s3 default
+# system logger
+emerge --ask app-admin/sysklogd
+# enable
+rc-update add sysklogd default
+# cron daemon
+emerge --ask sys-process/cronie
+rc-update add cronie default
+# file indexing
+emerge --ask sys-apps/mlocate
+# time synchronize
+emerge --ask net-misc/chrony
+rc-update add chronyd default
+# grub
+emerge --ask --verbose sys-boot/grub
+echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
+# install
+grub-install --target=x86_64-efi --efi-directory=/boot
+# if error
+# mount -o remount,rw /sys/firmware/efi/efivars
+grub-install --target=x86_64-efi --efi-directory=/boot --removable
+# config 
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# reboot
+exit
+cd
+umount -l /mnt/gentoo/dev{/shm,/pts,}
+umount -R /mnt/gentoo
+reboot
